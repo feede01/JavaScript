88b142398b6656bee1db3fc34ae16acf6ecdf482
feede01 <fededortenzio@ufasta.edu.ar>
2022-11-15 22:13:56 -0300

    Proyecto final JS

--- js/carrito.js
+++ js/carrito.js
@@ -1,48 +1,49 @@
 const carrito = []
 
+const loader = () => `<img src="imagenes/loading.gif" alt="Cargando" width="35px">`
 
 const recuperarCarrito = ()=> {
     if (localStorage.getItem("carrito")) {
-        let carritoRecuperado = JSON.parse(localStorage.getItem("carrito")) || []
-            carritoRecuperado.forEach(producto => carrito.push(producto))
+        let carritoGuardado = JSON.parse(localStorage.getItem("carrito")) || []
+            carritoGuardado.forEach(producto => carrito.push(producto))
     } else {
-        console.warn("No se encontró un carrito previamente guardado.")
+        console.warn("No existe carrito guardado anteriormente.")
     }
 }
 
 const cargarCarrito = () => {
     let tablaBody = ""
     const tbody = document.querySelector("tbody")
-          tbody.innerHTML = ""
-          carrito.forEach(producto => {
-            tablaBody += `<tr>
-                            <th class="centrar">${producto.descripcion}</th>
-                            <th>${producto.nombre}</th>
-                            <th>$ ${producto.precio}</th>
-                            <th>Un pago</th>
-                            <th><button id="${producto.nombre}" class="button-delete button-small">QUITAR</button></th>
-                        </tr>`
-          })
-          tbody.innerHTML = tablaBody
-    let totalCarrito = carrito.reduce((acc, item)=> acc + item.precio, 0 )
-        tbody.innerHTML += `<tr>
-                                <th></th>
-                                <th>TOTAL</th>
-                                <th>$ ${totalCarrito.toFixed(2)}</th>
-                            </tr>`
-        activoBotonesParaQuitar()
+    tbody.innerHTML = ""
+    carrito.forEach(producto => {
+        tablaBody += `<tr>
+                <th class="centrar">${producto.descripcion}</th>
+                <th>${producto.nombre}</th>
+                <th>$ ${producto.precio}</th>
+                <th><input type="number" id="cuotas" value="0" min="0" max="18" step="3"></th>
+                <th><button id="${producto.nombre}" class="button-delete button-small">QUITAR</button></th>
+            </tr>`
+    })
+    tbody.innerHTML = tablaBody
+    let sumaTotal = carrito.reduce((acc, producto)=> acc + producto.precio, 0 )
+    tbody.innerHTML += `<tr>
+        <th></th>
+        <th>TOTAL</th>
+        <th>$ ${sumaTotal.toFixed(2)}</th>
+    </tr>`
+    activoBotonesParaQuitar()
 }
 
 const activoBotonesParaQuitar = ()=> {
     const botonQuitar = document.querySelectorAll(".button-delete.button-small")
-          botonQuitar.forEach(btn => {
-            btn.addEventListener("click", (e)=> {
-                let aEliminar = carrito.findIndex(producto => producto.nombre === e.target.id) 
-                    carrito.splice(aEliminar, 1)
-                    localStorage.setItem("carrito", JSON.stringify(carrito))  
-                    cargarCarrito()  
-            })
-          })
+    botonQuitar.forEach(btn => {
+        btn.addEventListener("click", (e)=> {
+            let Elimina = carrito.findIndex(producto => producto.nombre === e.target.id) 
+            carrito.splice(Elimina, 1)
+            localStorage.setItem("carrito", JSON.stringify(carrito))  
+            cargarCarrito()  
+        })
+    })
 }
 
 const alerta = (titulo, textoBoton)=> {
@@ -54,13 +55,19 @@ const carritoVacio = ()=> {
 }
 
 const finalizarCompra = ()=> {
-    alerta('Gracias por su compra, vuelva pronto', 'Confirmar').then(result => {
+    btnComprar.innerHTML = loader()
+    setTimeout(() => {
+    alerta('Gracias por su compra, vuelva pronto', 'Confirmar')
+    .then(result => {
         if (result.isConfirmed) {
             localStorage.removeItem("carrito")
             location.href = 'index.html'
         }
     })
+    }, 2500);
 }
+
+
 
 btnComprar.addEventListener("click", ()=> carrito.length === 0 ? carritoVacio() : finalizarCompra() )
 
--- js/miarchivo.js
+++ js/miarchivo.js
@@ -3,45 +3,74 @@ const contenido = document.getElementById("principal")
 const verCarrito = document.getElementById("verCarrito")
 
 const carrito = []
+let productos = []
 
-productos.forEach((producto)=>{
-    let content = document.createElement("div");
-    content.className = "card"
-    content.innerHTML = `
-        <img class= "card-image" src = "${producto.img}">
-        <div class = "card-name">${producto.nombre}</h3>
-        <div class = "card-desciption">${producto.descripcion}</h3>
-        <p class="card-price">${producto.precio} $</p>
-    `;
-    contenido.append(content);
-    let comprar = document.createElement("button");
-    comprar.innerText = "Agregar a 🛒"
-    comprar.className = "button"
+const toast = ()=> {
+    Toastify({
+        text: 'Elemento agregado al carrito',
+        duration: 1500,
+        close: true,
+        gravity: "top",
+        position: "right", 
+        style: {
+          background: "#9b4dca",
+          width: "275px",
+        }
+      }).showToast();
+}
 
-    content.append(comprar)
+const cargarMisProductos = async ()=>{
+    try {
+        const response = await fetch("../bbdd/productos.json")
+        productos = await response.json()
+        productos.forEach((producto)=>{
+            let content = document.createElement("div");
+            content.className = "card"
+            content.innerHTML = `
+                <img class= "card-image" src = "${producto.img}">
+                <div class = "card-name">${producto.nombre}</h3>
+                <div class = "card-desciption">${producto.descripcion}</h3>
+                <p class="card-price">${producto.precio} $</p>
+            `;
+            contenido.append(content);
+            let comprar = document.createElement("button");
+            comprar.innerText = "Agregar a 🛒"
+            comprar.className = "button"
+            content.append(comprar)
+            content.addEventListener("click", () =>{
+                carrito.push({
+                    img : producto.img,
+                    nombre : producto.nombre,
+                    descripcion : producto.descripcion,
+                    precio : producto.precio,
+                });
+                guardaCarrito()
+                toast()
+            });
+    })
+    } catch (error) {
+        swal({
+            title: "Hubo un error cargando los productos",
+            text: "Cargue nuevamente la página o espere a que el equipo lo solucione",
+            icon: "warning",
+            buttons: true,
+            dangerMode: true,
+        })
+    }
+}
 
-    content.addEventListener("click", () =>{
-        carrito.push({
-            img : producto.img,
-            nombre : producto.nombre,
-            descripcion : producto.descripcion,
-            precio : producto.precio,
-        });
-        guardarCarrito()
-    });
-})
-
-const guardarCarrito = () =>{
+const guardaCarrito = () =>{
     carrito.length > 0 && localStorage.setItem("carrito", JSON.stringify(carrito))
 }
 
 const recuperarCarrito = ()=> {
     if (localStorage.getItem("carrito")) {
-        let carritoRecuperado = JSON.parse(localStorage.getItem("carrito"))
-            carritoRecuperado.forEach(producto => carrito.push(producto))
+        let carritoGuardado = JSON.parse(localStorage.getItem("carrito"))
+            carritoGuardado.forEach(producto => carrito.push(producto))
     } else {
-        console.warn("No se encontró un carrito previamente guardado.")
+        console.warn("No existe carrito guardado anteriormente.")
     }
 }
 
 recuperarCarrito()
+cargarMisProductos()
